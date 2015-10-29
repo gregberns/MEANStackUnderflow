99adf9d59821e4e2642cf16c92caa548fd99e4c1
Greg Berns <gberns>
2015-10-29 10:44:01 -0700

    changed name

--- models/questions2.js
+++ models/questions2.js
@@ -23,28 +23,5 @@ questionsSchema.methods.downvote = function(cb) {
 	this.votes -= 1;
 	this.save(cb);
 };
-// 
-// questionsSchema.pre('remove', function(nextAction){
-// 	var question = this;
-// 	
-// 	question.model('Class').update({
-// 		_id: { $in: question.classes }
-// 		},{
-// 			$pull: { questions: question._id }
-// 		},{
-// 			multi: true
-// 		},
-// 		next
-// 	);
-// 	
-// 	Async.each(question.answers, function(subId, next){
-// 		answerSchema.findById(subId).exec(function(err, answer){
-// 			answer.remove();
-// 			next();
-// 		}, function(){
-// 			nextAction();
-// 		})
-// 	})
-// })
 
 mongoose.model('questions', questionsSchema);
